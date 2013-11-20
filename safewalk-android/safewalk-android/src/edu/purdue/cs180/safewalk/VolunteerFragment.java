8924e708132988be1d90368fbc2abef8fd123167
Michael Reed <michael.reed315@gmail.com>
2013-11-20 13:13:59 -0500

    Added code for VolunteerFragment for Android app

--- safewalk-android/safewalk-android/src/edu/purdue/cs180/safewalk/VolunteerFragment.java
+++ safewalk-android/safewalk-android/src/edu/purdue/cs180/safewalk/VolunteerFragment.java
@@ -20,8 +20,8 @@ import android.widget.TextView;
  *
  * INSTRUCTIONS: Study RequesterFragment.java and fill in comparable code where TODO markers are.
  * 
- * @author YOU
- * @author YOUR-PARTNER
+ * @author reed136
+ * @author duffy10
  */
 @SuppressLint("DefaultLocale")
 public class VolunteerFragment extends Fragment implements ChannelListener, View.OnClickListener, Observer, OnItemSelectedListener {
@@ -43,13 +43,15 @@ public class VolunteerFragment extends Fragment implements ChannelListener, View
         locations = (Spinner) rootView.findViewById(R.id.locations_spinner);
 
         // TODO: add this Fragment object to the Model observer and AndroidChannel listener...
+        model.addObserver(this);
+        channel.setChannelListener(this);
 
         // TODO: set/restore the UI state from the model...
+        update(model, null);
 
         // TODO: add listeners for the Volunteer button and spinner...
         button.setOnClickListener(this);
-
-
+        locations.setOnItemSelectedListener(this);
         return rootView;
     }
     
@@ -60,6 +62,7 @@ public class VolunteerFragment extends Fragment implements ChannelListener, View
     public void onDestroyView() {
         super.onDestroyView();
         // TODO: add code to delete this Observer from the model
+        model.deleteObserver(this);
     }
 
     /**
@@ -70,8 +73,16 @@ public class VolunteerFragment extends Fragment implements ChannelListener, View
         status.setText("Volunteer not implemented yet"); // TODO Delete this line (!)
 
         // TODO: disable controls until request assigned (or failure sent)...
+        model.setUIEnabled(false);
+        model.setStatus("Sending message to server; waiting for requester");
+        model.notifyObservers();
 
         // TODO: prepare message and send to server...
+        String selectedItem = (String) locations.getSelectedItem();
+        String[] words = selectedItem.split(" ");
+        String message = "VOLUNTEER " + words[0];
+        System.out.printf("Sending: %s\n", message);
+        channel.sendMessage(message);
     }
 
     /**
@@ -80,6 +91,10 @@ public class VolunteerFragment extends Fragment implements ChannelListener, View
     @Override
     public void update(Observable observable, Object data) {
         // TODO: using data from the model, update the UI widgets (locations, button, and status)
+    	locations.setSelection(model.getLocation(), true);
+    	button.setEnabled(model.isUIEnabled());
+    	locations.setEnabled(model.isUIEnabled());
+    	status.setText(model.getStatus());
     }
 
     /**
@@ -87,8 +102,17 @@ public class VolunteerFragment extends Fragment implements ChannelListener, View
      * Volunteer application, so the only message it knows about it is the LOCATION message: "LOCATION id urgency".
      */
     public void messageReceived(String message) {
-        // TODO: handle message from server (only valid one is "LOCATION ID URGENCY") by updating model
+        // TODO: handle message from server (only valid one is "LOCATION BLDG URGENCY") by updating model
         // TODO: don't forget to notify observers
+    	String[] fields = message.split(" ");
+    	
+    	if (fields[0].equals("LOCATION")) {
+    		model.setUIEnabled(true);
+    		model.setStatus(String.format("Proceed to %s with urgency %s", fields[1], fields[2]));
+    	}
+    	else
+    		model.setStatus("Unexpected message: " + message);
+    	model.notifyObservers();
     }
 
     /**
@@ -97,6 +121,10 @@ public class VolunteerFragment extends Fragment implements ChannelListener, View
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
         // TODO: when an item is selected on the spinner, update the model to keep track of it
+    	if (arg0 == locations)
+    		model.setLocation(arg2);
+    	else
+    		System.err.printf("ERROR: Unexpected spinner (%s)\n", arg0.toString());
     }
 
     /**
@@ -113,5 +141,8 @@ public class VolunteerFragment extends Fragment implements ChannelListener, View
     public void notifySendFailure() {
         // TODO: the network connection is down or server unavailable; update status message in model
         // TODO: don't forget to notify observers
+    	model.setStatus("Error sending message to server; check server status and network connection");
+        model.setUIEnabled(true);
+        model.notifyObservers();
     }
 }
