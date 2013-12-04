b9107cc91f118f49c6f1e22e1eb96fc6d47b26d5
Michael Reed <michael.reed315@gmail.com>
2013-12-03 20:04:13 -0500

    Edited Server for project 8 version (final version)

--- project8/Server.java
+++ project8/Server.java
@@ -15,15 +15,16 @@ public class Server implements MessageListener {
     private Channel channel;
     private ArrayList<Request> requesters = new ArrayList<Request>();
     private ArrayList<Volunteer> volunteers = new ArrayList<Volunteer>();
-    private String algorithm;
+    private int interval;
     private final int[][] timeValues = {{0, 8, 6, 5, 4}, {8, 0, 4, 2, 5}, {6, 4, 0, 3, 1},
         {5, 2, 3, 0, 7}, {4, 5, 1, 7, 0}};
     private HashMap<String, Integer> buildings = new HashMap<String, Integer>();
+    Object lock = new Object();
     
-    public Server(Channel channel, String algorithm) {
+    public Server(Channel channel, int interval) {
         this.channel = channel;
         this.channel.setMessageListener(this);
-        this.algorithm = algorithm;
+        this.interval = interval;
         buildings.put("CL50", 0);
         buildings.put("EE", 1);
         buildings.put("LWSN", 2);
@@ -76,10 +77,12 @@ public class Server implements MessageListener {
     public static void main(String[] args) {
         try {
             int port = Integer.parseInt(args[0]);
-            Server server = new Server(new TCPChannel(port), args[1]);
+            int interval = Integer.parseInt(args[1]);
+            Server server = new Server(new TCPChannel(port), interval);
+            server.run();
         }
         catch (NumberFormatException e) {
-            System.out.println("First argument must be an integer");
+            System.out.println("Arguments must be integers.");
             System.exit(0);
         }
     }
@@ -87,14 +90,57 @@ public class Server implements MessageListener {
     @Override
     public void messageReceived(String message, int clientID) {
         addClient(message, clientID);
-        if (algorithm.equals("FCFS")) {
-            fcfs();
-        }
-        else if (algorithm.equals("CLOSEST")) {
-            sendClosest();
-        }
-        else if (algorithm.equals("URGENCY")) {
-            sendUrgency();
+    }
+    
+    public void run() {
+        Request chosenRequester;
+        Volunteer chosenVolunteer;
+        while (true) {
+            try {
+                Thread.sleep(interval);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (lock) {
+                while ((!requesters.isEmpty()) && (!volunteers.isEmpty())) {
+                    chosenVolunteer = null;
+                    chosenRequester = null;
+                    ArrayList<Request> emergency = new ArrayList<Request>();
+                    ArrayList<Request> urgent = new ArrayList<Request>();
+                    ArrayList<Request> normal = new ArrayList<Request>();
+                    for (Request r : requesters) {
+                        if (r.getUrgency().equals("EMERGENCY")) {
+                            emergency.add(r);
+                        }
+                        else if (r.getUrgency().equals("URGENT")) {
+                            urgent.add(r);
+                        }
+                        else if (r.getUrgency().equals("NORMAL")) {
+                            normal.add(r);
+                        }
+                    }
+                    if (!emergency.isEmpty()) {
+                        chosenRequester = emergency.get(0);
+                    }
+                    else if (!urgent.isEmpty()) {
+                        chosenRequester = urgent.get(0);
+                    }
+                    else if (!normal.isEmpty()) {
+                        chosenRequester = normal.get(0);
+                    }
+                    String fixedLocation = chosenRequester.getLocation();
+                    int minDistance = 10;
+                    int distance;
+                    for (Volunteer v : volunteers) {
+                        distance = timeValues[buildings.get(v.getLocation())][buildings.get(fixedLocation)];
+                        if (distance < minDistance) {
+                            minDistance = distance;
+                            chosenVolunteer = v;
+                        }
+                    }
+                    sendMessage(chosenVolunteer, chosenRequester);
+                }
+            }
         }
     }
     
@@ -191,12 +237,22 @@ public class Server implements MessageListener {
     }
     
     public void addClient(String message, int clientID) {
-        String[] m = message.split(" ");
-        if (message.charAt(0) == 'R') {
-            requesters.add(new Request(m[1], m[2], clientID));
+        synchronized (lock) {
+            String[] m = message.split(" ");
+            if (message.charAt(0) == 'R') {
+                requesters.add(new Request(m[1], m[2], clientID));
+            }
+            else if (message.charAt(0) == 'V') {
+                volunteers.add(new Volunteer(m[1], clientID));
+            }
         }
-        else if (message.charAt(0) == 'V') {
-            volunteers.add(new Volunteer(m[1], clientID));
-        }
+    }
+    
+    public int getNumberRequesters() {
+        return requesters.size();
+    }
+    
+    public int getNumberVolunteers() {
+        return volunteers.size();
     }
 }
